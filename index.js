a35dd92f395107c5e4b551230597e1656698192b
Saif-Rahman-801 <saifurs102@gmail.com>
2023-12-27 18:01:31 +0600

    app object

---
+++ index.js
@@ -0,0 +1,45 @@
+import express, { Router } from "express";
+const app = express();
+const admin = express()
+const PORT = 5000;
+
+app.use(express.json()); // for Content-Type: "application/json()";
+
+
+/* app.use(
+  static(__dirname + "/public/", {
+    index: "home.html",
+  })
+); */ // to use satatic data from your project file (you can access images, text file or any kind of static data), you can give a option object, that will be a root folder that will be shown if no route is added.
+
+const router = Router(); // it returns a router object
+app.use(router); // your express app can use it
+
+// now you can use router.get/post/delete etc instead of app.get/post/delete
+
+
+
+
+router.get("/router", (req, res) => {
+  res.send("Hello from router");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+admin.get("/dashboard", (req, res) => {
+  console.log(admin.mountpath);
+  res.send("Welcome to Dashboard");
+});
+
+
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.send("Post api");
+});
+
+app.listen(PORT, () => {
+  console.log("app listening on ", PORT);
+});
